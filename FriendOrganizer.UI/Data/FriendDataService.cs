a7b3489db35d560027313dbfe7ff63236f865e09
Lawrence LCI <lcheunglci@gmail.com>
2022-05-04 01:09:23 -0700

    Add the FriendDataService

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from a real database.
+            yield return new Friend { FirstName = "Bob", LastName = "Doe" };
+            yield return new Friend { FirstName = "Alice", LastName = "Jackson" };
+            yield return new Friend { FirstName = "Carl", LastName = "Smith" };
+            yield return new Friend { FirstName = "Tony", LastName = "Stark" };
+        }
+    }
+}
