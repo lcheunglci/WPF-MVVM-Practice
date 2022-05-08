952003fc25e306b3cc8c9caf03110a92d777e593
Lawrence LCI <lcheunglci@gmail.com>
2022-05-08 00:04:13 -0700

    Add DbContext subclass and EF6 into DataAccess

---
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FriendOrganizer.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FriendOrganizer.DataAccess
+{
+    class FriendOrganizerDbContext : DbContext
+    {
+        public FriendOrganizerDbContext() : base("FriendOrganizerDb")
+        {
+
+        }
+
+        public DbSet<Friend> Friends { set; get; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
