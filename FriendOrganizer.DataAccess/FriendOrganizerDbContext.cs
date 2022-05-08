e29b779c065d964e355e9e569503ad4105a2760e
Lawrence LCI <lcheunglci@gmail.com>
2022-05-08 00:17:44 -0700

    Change from EF6 to EFCore

--- FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -1,22 +1,10 @@
 ﻿using FriendOrganizer.Model;
-using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
+using Microsoft.EntityFrameworkCore;
 
 namespace FriendOrganizer.DataAccess
 {
     class FriendOrganizerDbContext : DbContext
     {
-        public FriendOrganizerDbContext() : base("FriendOrganizerDb")
-        {
-
-        }
-
-        public DbSet<Friend> Friends { set; get; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }
+        public DbSet<Friend> Friend { set; get; }
     }
 }
