f944037d6e6926ad0eba70d0a5dde2725888c5c7
Lawrence LCI <lcheunglci@gmail.com>
2022-05-10 23:53:07 -0700

    Update the dbcontext so it uses the sqlserver mssqllocaldb provided by vs

--- FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -3,8 +3,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FriendOrganizer.DataAccess
 {
-    class FriendOrganizerDbContext : DbContext
+    public class FriendOrganizerDbContext : DbContext
     {
-        public DbSet<Friend> Friend { set; get; }
+        public DbSet<Friend> Friends { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=FriendOrganizerDb;Initial Catalog=FriendOrganizer;Integrated Security=true;Trusted_Connection=true");
+        }
     }
 }
