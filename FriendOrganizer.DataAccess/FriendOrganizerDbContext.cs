70026d55c3932e82004c1523c67de8406ff21883
Lawrence LCI <lcheunglci@gmail.com>
2022-05-11 23:36:24 -0700

    Add migration for adding data seed

--- FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
+++ FriendOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -12,5 +12,17 @@ namespace FriendOrganizer.DataAccess
             optionsBuilder.UseSqlServer(
                 @"Server=(localdb)\mssqllocaldb;Database=FriendOrganizerDb;Initial Catalog=FriendOrganizer;Integrated Security=true;Trusted_Connection=true");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Friend>().HasData(
+                new Friend { Id = 1, FirstName = "Thomas", LastName = "Anderson", Email = "thomasa@testfriendapp.com" },
+                new Friend { Id = 2, FirstName = "Chuck", LastName = "Norris", Email = "chuckn@testfriendapp.com" },
+                new Friend { Id = 3, FirstName = "Bruce", LastName = "Lee", Email = "brucel@testfriendapp.com" },
+                new Friend { Id = 4, FirstName = "Jackie", LastName = "Chan", Email = "jackiec@testfriendapp.com" },
+                new Friend { Id = 5, FirstName = "Jet", LastName = "Li", Email = "jetl@testfriendapp.com" }
+                );
+        }
     }
 }
---
+++ FriendOrganizer.DataAccess/Migrations/20220512063549_AddDataSeed.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FriendOrganizer.DataAccess.Migrations
+{
+    public partial class AddDataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Friends",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "thomasa@testfriendapp.com", "Thomas", "Anderson" },
+                    { 2, "chuckn@testfriendapp.com", "Chuck", "Norris" },
+                    { 3, "brucel@testfriendapp.com", "Bruce", "Lee" },
+                    { 4, "jackiec@testfriendapp.com", "Jackie", "Chan" },
+                    { 5, "jetl@testfriendapp.com", "Jet", "Li" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Friends",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
